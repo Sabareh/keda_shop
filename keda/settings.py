5e15d96b4d035bc11fa979a278b011ac8bb56c3e
Victor Oketch Sabare <sabarevictor@gmail.com>
2023-01-15 19:49:13 +0300

    Continuing with this shit

--- keda/settings.py
+++ keda/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
-    'store',
-    
+    'store',  
 ]
 
 MIDDLEWARE = [
@@ -66,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links',
             ],
         },
     },
