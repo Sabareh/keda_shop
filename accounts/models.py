c9172fca057580870f652c6ef9de67225f84063d
Victor Oketch Sabare <sabarevictor@gmail.com>
2023-01-15 08:54:45 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -7,11 +7,53 @@ from django.utils import timezone
 
 # Create your models here.
 
+class MyAccountManager(BaseUserManager):
+    def create_user(self, email, username, first_name, last_name, phone_number, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have a username")
+        if not first_name:
+            raise ValueError("Users must have a first name")
+        if not last_name:
+            raise ValueError("Users must have a last name")
+        if not phone_number:
+            raise ValueError("Users must have a phone number")
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            phone_number = phone_number,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, first_name, last_name, phone_number, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            password = password,
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            phone_number = phone_number,
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 class Account(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=50, blank=True)
-    last_name = models.CharField(_('last name'), max_length=50, blank=True)
-    phone_number = models.CharField(_('phone number'), max_length=50, blank=True)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone_number = models.CharField(max_length=50, blank=True)
 
     #required fields
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -21,13 +63,16 @@ class Account(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','first_name', 'last_name', 'phone_number']
+
     def __str__(self):
         return self.email
 
     def has_perm(self, perm, obj=None):
         return self.is_admin
 
-    def has_module_perms(self, app_label):
+    def has_module_perms(self, add_label):
         return True
 
 
