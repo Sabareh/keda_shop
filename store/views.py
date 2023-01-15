9af3767ddad90c7ad9e3e5568ee28fce8c6cdad4
Victor Oketch Sabare <sabarevictor@gmail.com>
2023-01-15 15:08:12 +0300

    tired

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+
+def store(request,):
+    products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products': products
+    }
+    return render(request, 'store/store.html', context)
