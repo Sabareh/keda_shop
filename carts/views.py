fd1472f17c20f9cec12f58a052402051e178499d
Victor Oketch Sabare <sabarevictor@gmail.com>
2023-01-17 18:58:59 +0300

    Working on the orders

--- carts/views.py
+++ carts/views.py
@@ -27,8 +27,7 @@ def add_cart(request, product_id):
                 value = request.POST[key]
 
                 try:
-                    variation = Variation.objects.get(
-                        product=product, variation_category__iexact=key, variation_value__iexact=value)
+                    variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
                     product_variation.append(variation)
                 except:
                     pass
@@ -80,8 +79,7 @@ def add_cart(request, product_id):
                 value = request.POST[key]
 
                 try:
-                    variation = Variation.objects.get(
-                        product=product, variation_category__iexact=key, variation_value__iexact=value)
+                    variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
                     product_variation.append(variation)
                 except:
                     pass
